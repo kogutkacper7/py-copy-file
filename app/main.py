cd958c47f8dc1bc1481acbbed6a99d1a7852697d
Kacper Kogut <kogutkacper7@gmail.com>
2025-09-27 15:10:09 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
-# write your code here
+def copy_file(command: str) -> None:
+    split_command = command.split(" ")
+    if len(split_command) == 3 and split_command[0] == "cp":
+        if not split_command[2] == split_command[1]:
+            try:
+                with (open(split_command[1])
+                      as src, open(split_command[2], "w") as dst):
+                    dst.write(src.read())
+            except FileNotFoundError:
+                return
